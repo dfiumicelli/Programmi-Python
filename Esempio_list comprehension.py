93dfc20e5b411303e822caec7bb1e350c1be7a60
Daniele Fiumicelli <dfiumicelli@gmail.com>
2024-03-15 15:48:08 +0100

    .

---
+++ Esempio_list comprehension.py
@@ -0,0 +1,5 @@
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+filtered_list = [i for i in num_list if i > 2]
+print(filtered_list)
+transformed_list = [i**2 for i in num_list]
+print(transformed_list)
---
+++ Seconda_Lezione_if_else.py
@@ -0,0 +1,11 @@
+input_value = int(input("Inserisci un numero tra 0 e 3\n"))
+if input_value == 0:
+    print("Valore è 0")
+elif input_value == 1:
+    print("Valore è 1")
+elif input_value == 2:
+    print("Valore è 2")
+elif input_value == 3:
+    print("Valore è 3")
+else:
+    print("Errore")
