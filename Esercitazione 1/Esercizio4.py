ab2d7874eca97573baaa20e6f32d6c143ac0d3e9
Daniele Fiumicelli <dfiumicelli@gmail.com>
2024-03-26 16:51:01 +0100

    .

--- Esercitazione 1/Esercizio4.py
+++ Esercitazione 1/Esercizio4.py
@@ -1,3 +1,9 @@
 ls = []
 for i in range(4):
     ls.append(int(input("Inserisci un numero ")))
+if ls[0] < ls[1] < ls[2] < ls[3]:
+    print("Sequenza strettamente crescente")
+elif ls[0] > ls[1] > ls[2] > ls[3]:
+    print("Sequenza strettamente decrescente")
+else:
+    print("Sequenza mista")
