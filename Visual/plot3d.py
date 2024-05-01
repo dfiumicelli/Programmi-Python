c38ab23dfe427bbd9587344ab0163a67caa42393
dfiumicelli <dfiumicelli@gmail.com>
2024-05-01 11:22:22 +0200

    .

--- Visual/plot3d.py
+++ Visual/plot3d.py
@@ -5,19 +5,20 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+
 sns.set()
-fig=plt.figure()
+fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-zline = 2*np.linspace(0, 15, 1000)
+zline = 2 * np.linspace(0, 15, 1000)
 xline = np.sin(zline)
 yline = np.cos(zline)
-ax.plot3D(xline,yline,zline,'gray')
+ax.plot3D(xline, yline, zline, 'gray')
 
 # mostriamo anche uno scatter
 
-zdata = 2*15*np.random.random(1000)
-xdata = np.sin(zdata) + 0.1*np.random.randn(1000)
-ydata = np.cos(zdata) + 0.1*np.random.randn(1000)
-ax.scatter3D(xdata,ydata,zdata, c=zdata, cmap='Greens')
+zdata = 2 * 15 * np.random.random(1000)
+xdata = np.sin(zdata) + 0.1 * np.random.randn(1000)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(1000)
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
 plt.show()
