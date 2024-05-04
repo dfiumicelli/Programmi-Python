786730e9cc2c0d0503b494bb9b2308ad8f93ebab
dfiumicelli <dfiumicelli@gmail.com>
2024-05-04 14:35:22 +0200

    .

--- Esercitazione Numpy/Esercizio10.py
+++ Esercitazione Numpy/Esercizio10.py
@@ -3,5 +3,4 @@ import numpy as np
 a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 b = np.array([1, 2, 30, 40, 50, 60, 70, 8, 9, 10])
 
-print("Valori comuni: ")
-
+print("Valori comuni: ", np.intersect1d(a, b))
